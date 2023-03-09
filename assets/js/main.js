6899b648c3d6d9e4cbe557494f455336373b250d
Brian Weithers <36247821+bweithers@users.noreply.github.com>
2023-03-09 10:50:57 -0600

    Added and Hyperplexed the title

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 var left = null;
 var centerPane = null;
 var lock = 0;
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 const handleOnClick = e => {
     if (e.target.id != 'right-side' && e.target.id != 'left-side'){
@@ -23,6 +24,7 @@ const handleOnMove = e => {
     }
     var p = e.clientX / window.innerWidth * 100;
     if (e.clientY > 1000){p=100;}
+    console.log(left.style.top + left.style.height);
     if (p < 0.5){
         p=0.0;
     }
@@ -43,4 +45,46 @@ document.onmousemove = e => {
 
 document.ontouchmove = e => {
     handleOnMove(e);
+}
+
+// document.getElementById("kwargs-link").onmouseover = event => {
+//     let iterations = 0;
+//     const interval = setInterval(() => {
+//         event.target.innerText = event.target.innerText.split("")
+//         .map(letter => letters[Math.floor(Math.random() * 26)])
+//         .join("");
+
+//         if (iterations++ >= 20) clearInterval(interval);
+//         iterations++;
+//     }
+//     ,50);
+// }
+
+
+
+let interval = null;
+
+document.getElementById("kwargs-link").onmouseover = event => {  
+  let iteration = 0;
+  
+  clearInterval(interval);
+  
+  interval = setInterval(() => {
+    event.target.innerText = event.target.innerText
+      .split("")
+      .map((letter, index) => {
+        if(index+1 < iteration) {
+          return event.target.dataset.value[index];
+        }
+      
+        return letters[Math.floor(Math.random() * 26)]
+      })
+      .join("");
+    
+    if(iteration >= event.target.dataset.value.length){ 
+      clearInterval(interval);
+    }
+    
+    iteration += 1 / 3;
+  }, 30);
 }
