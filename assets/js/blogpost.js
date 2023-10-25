4df33482a00ef02c3afb1948d3efdb61efe64c49
Brian Weithers <36247821+bweithers@users.noreply.github.com>
2023-10-25 11:16:36 -0500

    rescrambled kwargs link on leave, always keep dot

--- assets/js/blogpost.js
+++ assets/js/blogpost.js
@@ -12,6 +12,7 @@ document.getElementById("kwargs-link").onmouseover = event => {
     event.target.innerText = event.target.innerText
       .split("")
       .map((letter, index) => {
+        if (letter == '.'){return '.'}
         if(index+1 < iteration) {
           return event.target.dataset.value[index];
         }
@@ -21,6 +22,30 @@ document.getElementById("kwargs-link").onmouseover = event => {
       .join("");
     
     if(iteration >= event.target.dataset.value.length){ 
+      clearInterval(interval);
+    }
+    
+    iteration += 1 / 3;
+  }, 30);
+}
+document.getElementById("kwargs-link").onmouseleave = event => {  
+  let iteration = 0;
+  clearInterval(interval);
+  
+  interval = setInterval(() => {
+    event.target.innerText = event.target.innerText.split("").map(
+      (letter, index) => {
+        if (letter == '.'){return '.'}
+        if(event.target.innerText.length - index + 1 < iteration) {
+          return letters[Math.floor(Math.random() * letters.length)]
+          
+        }
+
+        return event.target.dataset.value[index];
+      })
+      .join("");
+    
+    if(iteration > event.target.dataset.value.length + 1){ 
       clearInterval(interval);
     }
     
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 var left = null;
 var centerPane = null;
 var lock = 0;
-const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*|";
 
 const handleOnClick = e => {
     if (e.target.id != 'right-side' && e.target.id != 'left-side'){
@@ -74,15 +74,41 @@ document.getElementById("kwargs-link").onmouseover = event => {
     event.target.innerText = event.target.innerText
       .split("")
       .map((letter, index) => {
+        if (letter == '.'){return '.'}
         if(index+1 < iteration) {
           return event.target.dataset.value[index];
         }
       
-        return letters[Math.floor(Math.random() * 26)]
+        return letters[Math.floor(Math.random() * letters.length)]
       })
       .join("");
     
     if(iteration >= event.target.dataset.value.length){ 
+      clearInterval(interval);
+    }
+    
+    iteration += 1 / 3;
+  }, 30);
+}
+
+document.getElementById("kwargs-link").onmouseleave = event => {  
+  let iteration = 0;
+  clearInterval(interval);
+  
+  interval = setInterval(() => {
+    event.target.innerText = event.target.innerText.split("").map(
+      (letter, index) => {
+        if (letter == '.'){return '.'}
+        if(event.target.innerText.length - index + 1 < iteration) {
+          return letters[Math.floor(Math.random() * letters.length)]
+          
+        }
+
+        return event.target.dataset.value[index];
+      })
+      .join("");
+    
+    if(iteration > event.target.dataset.value.length + 1){ 
       clearInterval(interval);
     }
     
