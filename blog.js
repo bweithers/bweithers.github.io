7f984f0e6646586ab764e69a88b2f0ccef08ad9d
BWeithers <36247821+bweithers@users.noreply.github.com>
2024-04-14 17:28:00 -0500

    added unchecking functionality

--- blog.js
+++ blog.js
@@ -3,14 +3,19 @@ console.log('In the js file');
 // Function to filter articles based on selected tag
 function filterArticles(tag) {
     const articles = document.querySelectorAll('.post');
-  
+    if (tag == 'all') {
+      articles.forEach(article => {
+        article.style.display = 'block';
+      });
+      return;
+    }
     articles.forEach(article => {
       const tags = article.querySelectorAll('.tag'); // Select all tags within the article
       let tagFound = false; // Flag to indicate if the tag is found within the article
   
       tags.forEach(tagElement => {
         const tagValue = tagElement.getAttribute('data-tag');
-        console.log(tagElement);
+        // console.log(tagElement);
         if (tagValue === tag) {
           tagFound = true; // Set flag to true if the tag is found
         }
@@ -30,3 +35,25 @@ document.querySelectorAll('.tags-menu li').forEach(tagLink => {
     filterArticles(selectedTag);
   });
 });
+
+// Event listener for tag links
+document.querySelectorAll('.tags-menu li').forEach(tagLink => {
+    tagLink.addEventListener('click', function(event) {
+      event.preventDefault();
+      const selectedTag = this.getAttribute('data-tag');
+      const isActive = this.getAttribute('data-active'); // Check if the tag is active
+        
+      console.log(isActive);
+      // Toggle the active class
+      if (isActive == "yes") {
+        this.setAttribute('data-active', 'no');
+        filterArticles('all'); // Show all articles when tag is unclicked
+      } else {
+        // Add active class to clicked tag link
+        this.setAttribute('data-active', 'yes');
+        // console.log(this.getAttribute('active'));
+        filterArticles(selectedTag); // Filter articles based on selected tag
+      }
+    });
+  });
+  
