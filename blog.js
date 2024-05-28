21cc6926516c1c2542691fe10e88347fb8bc4a97
BWeithers <36247821+bweithers@users.noreply.github.com>
2024-05-28 08:55:27 -0500

    Add in kwargs link feature

--- blog.js
+++ blog.js
@@ -61,3 +61,82 @@ document.querySelectorAll('.tags-menu li').forEach(tagLink => {
     });
   });
   
+  let interval = null;
+  // const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*|";
+  const letters = 'abcdefghijklmnopqrstuvwxyz';
+  // const cypher = 'brianw.gamer';
+  document.getElementById("kwargs-link").onmouseover = event => {  
+    let iteration = 0;
+    
+    clearInterval(interval);
+    
+    interval = setInterval(() => {
+      event.target.innerText = event.target.innerText
+        .split("")
+        .map((letter, index) => {
+          if (letter == '.'){return '.'}
+          if(index+1 < iteration) {
+            return event.target.dataset.value[index];
+          }
+        
+          return letters[Math.floor(Math.random() * letters.length)]
+        })
+        .join("");
+      
+      if(iteration >= event.target.dataset.value.length){ 
+        clearInterval(interval);
+      }
+      
+      iteration += 1 / 3;
+    }, 30);
+  }
+  
+  document.getElementById("kwargs-link").onmouseleave = event => {  
+    let iteration = 0;
+    clearInterval(interval);
+    
+    interval = setInterval(() => {
+      event.target.innerText = event.target.innerText.split("").map(
+        (letter, index) => {
+          if (letter == '.'){return '.'}
+          if(event.target.innerText.length - index + 1 < iteration) {
+            return letters[Math.floor(Math.random() * letters.length)]
+            
+          }
+  
+          return event.target.dataset.value[index];
+        })
+        .join("");
+      
+      if(iteration > event.target.dataset.value.length + 1){ 
+        clearInterval(interval);
+      }
+      
+      iteration += 1 / 3;
+    }, 30);
+  }
+  
+  document.getElementById("kwargs-link").onload = event => {  
+    let iteration = 0;
+    clearInterval(interval);
+    
+    interval = setInterval(() => {
+      event.target.innerText = event.target.innerText.split("").map(
+        (letter, index) => {
+          if(letter == '.'){return '.'}
+          if(event.target.innerText.length - index + 1 < iteration) {
+            return letters[Math.floor(Math.random() * letters.length)]
+
+          }
+  
+          return event.target.dataset.value[index];
+        })
+        .join("");
+      
+      if(iteration > event.target.dataset.value.length + 1){ 
+        clearInterval(interval);
+      }
+      
+      iteration += 1 / 3;
+    }, 30);
+  }
